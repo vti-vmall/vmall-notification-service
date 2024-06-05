a6060c9f39999030bb9e8643732643032ad91a25
tunk <nguyenkhactu1996@gmail.com>
2024-06-05 10:38:54 +0700

    Initial commit

---
+++ src/main/java/vn/edu/vti/vmall/vmall_notification_service/VmallNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallNotificationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VmallNotificationServiceApplication.class, args);
+  }
+
+}
